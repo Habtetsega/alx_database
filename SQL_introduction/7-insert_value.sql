54c1fb3191f9a7e7f080ed5886c7a2a1e5af82a4
Habtetsega <kido3871@gmail.com>
2023-08-28 23:28:00 -0700

    insert and count

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO `first_name`(`id`, `name`) VALUES(89, "Holberton School");
-
+-- Inserts a new row into the table first_table in my MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
