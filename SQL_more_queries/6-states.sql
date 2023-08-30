ae16695935d58a8b6e83791e2628cacdace96c3a
Habtetsega <kido3871@gmail.com>
2023-08-29 23:49:39 -0700

    create states table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT NOT NULL,name VARCHAR(256) NOT NULL,PRIMARY KEY(ID));
